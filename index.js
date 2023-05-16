f18c44252a2ae4e2294d55988e483b0e219598f5
Lam Ho <hophamlam92@yahoo.com>
2023-05-16 21:36:07 +0700

    finish auto deploy setup, change to staging run

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const cron = require("node-cron");
 const { sendDiscord } = require("./notification");
 console.log("index.js online");
 
-// const userData = require("./data/userData.sample.json");
-// const cronSchedule = userData.cronSchedule;
-// cron.schedule(cronSchedule, sendDiscord);
+const userData = require("./data/userData.sample.json");
+const cronSchedule = userData.cronSchedule;
+cron.schedule(cronSchedule, sendDiscord);
 
-// test cron job every minutes
-cron.schedule("* * * * *", sendDiscord);
-console.log("test cron job every minutes");
+// // test cron job every minutes
+// cron.schedule("* * * * *", sendDiscord);
+// console.log("test cron job every minutes");
